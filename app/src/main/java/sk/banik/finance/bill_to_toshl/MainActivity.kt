0702fba285ca8361709052fc277e5e09765c62ea
Michal Banik <banik.michal.sk@gmail.com>
2021-10-24 16:47:17 +0200

    init commit
    hello-world project
    
    Signed-off-by: Michal Banik <banik.michal.sk@gmail.com>

---
+++ app/src/main/java/sk/banik/finance/bill_to_toshl/MainActivity.kt
@@ -0,0 +1,11 @@
+package sk.banik.finance.bill_to_toshl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
